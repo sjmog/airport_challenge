3ee4610b6982207e6e9e1bb8e359d7ae02da37b3
Sam Morgan <s_morgan@me.com>
2015-10-19 17:25:51 +0100

    refactor of previous commit

--- lib/airport.rb
+++ lib/airport.rb
@@ -13,7 +13,7 @@ class Airport
 
   def take_off(plane)
     raise 'Cannot take off plane: weather is stormy' if stormy?
-    raise 'Cannot take off plane: plane not at this airport' unless @planes.include? plane
+    raise 'Cannot take off plane: plane not at this airport' unless at_airport?(plane)
   end
 
   private
@@ -24,5 +24,9 @@ class Airport
 
   def stormy?
     @weather_reporter.stormy?
+  end
+
+  def at_airport?(plane)
+    @planes.include? plane
   end
 end
