eee180fac62f1d89b73456a46ef060d70bb7d379
Sam Morgan <s_morgan@me.com>
2015-10-19 18:11:01 +0100

    As an air traffic controller So the system is consistent and correctly reports plane status and location I want to ensure a plane that has taken off from an airport is no longer in that airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,5 +1,6 @@
 class Airport
   DEFAULT_CAPACITY = 20
+  attr_reader :planes
 
   def initialize(opts)
     @capacity = opts.fetch(:capacity, DEFAULT_CAPACITY)
@@ -18,6 +19,7 @@ class Airport
     raise 'Cannot take off plane: weather is stormy' if stormy?
     raise 'Cannot take off plane: plane not at this airport' unless at_airport?(plane)
     plane.take_off
+    @planes.pop
     plane
   end
 
